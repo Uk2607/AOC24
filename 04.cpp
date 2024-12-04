868015f8a57435958ed54e5f37ea8fa74ef015bb
Utkarsh Kumawat <utkarsh2600@gmail.com>
2024-12-05 01:51:18 +0530

    code setup

--- 04.cpp
+++ 04.cpp
@@ -17,41 +17,22 @@ vector<string> get_input(string file_path) {
         return arr;
     }
 
-    while(getline(file, line)) arr.push_back(line+"###############################");  // added #'s to prevent string overflow
+    while(getline(file, line)) arr.push_back(line);
     file.close();
 
     return arr;
 }
 
-int get_num(string &s, int &i) {
-    int x = 0;
-    while(x<1000 && isdigit(s[i]))
-        x = x*10+(s[i++]-'0');
-    if(1<=x && x<=999) return x;
-    return -1;
+void part1(vector<string>arr) {
+    int res=0;
+    ;
+    cout<<"PART1: "<<res<<"\n";
 }
 
-void solve(vector<string>arr, bool part2 = false) {
+void part2(vector<string>arr) {
     int res=0;
-    bool enable = true;
-    for(string s: arr) {
-        int n = s.length();
-        for(int i=0;i<n;i++) {
-            if(part2 && s.substr(i, 4) == "do()") enable = true;
-            else if(part2 && s.substr(i, 7) == "don\'t()") enable = false;
-            if(enable && s.substr(i, 4)=="mul(") {
-                i+=4;
-                int x = get_num(s, i);
-                if(s[i]==',') {
-                    i+=1;
-                    int y = get_num(s, i);
-                    if(s[i]==')')
-                        if(x!=-1 && y!=-1) res+=(x*y);
-                }
-            }
-        }
-    }
-    cout<<"PART"<<(part2?2:1)<<": "<<res<<"\n";
+    ;
+    cout<<"PART2: "<<res<<"\n";
 }
 
 int main() {
@@ -59,6 +40,6 @@ int main() {
     cout<<"Enter file name: ";
     cin>>file_name;
     vector<string>arr  = get_input("input/"+file_name+".in");
-    solve(arr); // 184576302
-    solve(arr, true); // 118173507
+    part1(arr); // 
+    part2(arr); // 
 }
