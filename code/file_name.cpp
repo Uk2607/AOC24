2dafd7604df1a7b0eb85af5419c6075fb11526d6
Utkarsh Kumawat <utkarsh2600@gmail.com>
2024-12-12 10:41:54 +0530

    shorten the code

--- code/file_name.cpp
+++ code/file_name.cpp
@@ -3,14 +3,11 @@
 
 std::string get_file_name(int argc, char* argv[], std::string default_input) {
     std::string file_name;
-    if (argc > 1) {
-        file_name = argv[1];
-    } else {
+    if (argc > 1) file_name = argv[1];
+    else {
         std::cout << "Enter file name: ";
         std::getline(std::cin, file_name);
-        if (file_name.length() == 0) {
-            file_name = default_input;
-        }
+        if (file_name.length() == 0) file_name = default_input;
     }
     return file_name;
 }
