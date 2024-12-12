69ac015e7464b1d511f0e4a42cd975b6cd00cb9e
Utkarsh Kumawat <utkarsh2600@gmail.com>
2024-12-12 10:57:06 +0530

    day 11 p2 setup

--- code/11.cpp
+++ code/11.cpp
@@ -61,28 +61,13 @@ void part1(vector<ull>arr) {
 
 void part2(vector<ull>arr) {
     int blink = 75;
-    while (blink--) {
-        vector<ull>t;
-        for(ull x: arr) {
-            string str_x = to_string(x);
-            int l = str_x.length();
-            if(x==0) t.push_back(1);
-            else if(l%2==0) {
-                t.push_back(stoull(str_x.substr(0, l/2)));
-                t.push_back(stoull(str_x.substr(l/2)));
-            }
-            else {
-                t.push_back(x*2024);
-            }
-        }
-        arr = t;
-    }
+    ;
     cout<<"PART1: "<<arr.size()<<"\n";
 }
 
 int main(int argc, char* argv[]) {
     string file_name = get_file_name(argc, argv, "11");
     vector<ull>arr  = get_input("input/"+file_name+".in");
-    part1(arr); // 
+    part1(arr); // 203953
     part2(arr); // 
 }
