65fbb8931f621787a53f5371a23fafa4d69e83da
Utkarsh Kumawat <utkarsh2600@gmail.com>
2024-12-15 11:47:48 +0530

    day 15 p1

--- code/15.cpp
+++ code/15.cpp
@@ -52,9 +52,98 @@ void print(vector<string>&arr, vector<string>&ins) {
     cout<<"\n";
 }
 
+bool inside(int i, int j, int r, int c) {
+    return i>=0 && i<r && j>=0 && j<c;
+}
+
 void part1(vector<string>arr, vector<string>ins) {
-    print(arr, ins);
-    cout<<"PART1: "<<arr.size()<<"\n";
+    int r = arr.size(), c = arr[0].size();
+    pair<int,int>robot_pos;
+    vector<pair<int,int>>boxes;
+    for(int i=0;i<r;i++)
+        for(int j=0;j<c;j++) {
+            if(arr[i][j] == '@') {
+                robot_pos = make_pair(i, j);
+                arr[i][j] = '.';
+            }
+            else if(arr[i][j] == 'O') boxes.push_back(make_pair(i, j));
+        }
+
+    int nx, ny;
+    for(string row: ins) {
+        for(char dir: row) {
+            if(dir == '^') { // up
+                nx = robot_pos.first-1;
+                ny = robot_pos.second;
+                if(inside(nx, ny, r, c)) {
+                    if(arr[nx][ny] == '.') robot_pos.first -= 1;
+                    else {
+                        while(nx>=0 && arr[nx][ny]=='O') --nx;
+                        if(nx>=0 && arr[nx][ny] == '.') {
+                            arr[robot_pos.first-1][robot_pos.second] = '.';
+                            arr[nx][ny] = 'O';
+                            robot_pos.first -= 1;
+                        }
+                    }
+                }
+            } else if(dir == '>') { // right
+                nx = robot_pos.first;
+                ny = robot_pos.second+1;
+                if(inside(nx, ny, r, c)) {
+                    if(arr[nx][ny] == '.') robot_pos.second += 1;
+                    else {
+                        while(ny<c && arr[nx][ny]=='O') ++ny;
+                        if(ny<c && arr[nx][ny] == '.') {
+                            arr[robot_pos.first][robot_pos.second+1] = '.';
+                            arr[nx][ny] = 'O';
+                            robot_pos.second += 1;
+                        }
+                    }
+                }
+            } else if(dir == 'v') { // down
+                nx = robot_pos.first+1;
+                ny = robot_pos.second;
+                if(inside(nx, ny, r, c)) {
+                    if(arr[nx][ny] == '.') robot_pos.first += 1;
+                    else {
+                        while(nx<r && arr[nx][ny]=='O') ++nx;
+                        if(nx<r && arr[nx][ny] == '.') {
+                            arr[robot_pos.first+1][robot_pos.second] = '.';
+                            arr[nx][ny] = 'O';
+                            robot_pos.first += 1;
+                        }
+                    }
+                }
+            } else { // left
+                nx = robot_pos.first;
+                ny = robot_pos.second-1;
+                if(inside(nx, ny, r, c)) {
+                    if(arr[nx][ny] == '.') robot_pos.second -= 1;
+                    else {
+                        while(ny>=0 && arr[nx][ny]=='O') --ny;
+                        if(ny>=0 && arr[nx][ny] == '.') {
+                            arr[robot_pos.first][robot_pos.second-1] = '.';
+                            arr[nx][ny] = 'O';
+                            robot_pos.second -= 1;
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    int res = 0;
+    for(int i=0;i<r;i++) {
+        for(int j=0;j<c;j++) {
+            // cout<<arr[i][j];
+            if(arr[i][j] == 'O') {
+                res += (100*i)+j;
+            }
+        }
+        // cout<<"\n";
+    }
+
+    cout<<"PART1: "<<res<<"\n";
 }
 
 void part2(vector<string>arr, vector<string>ins) {
@@ -64,6 +153,6 @@ void part2(vector<string>arr, vector<string>ins) {
 int main(int argc, char* argv[]) {
     string file_name = get_file_name(argc, argv, "__");
     pair<vector<string>, vector<string>>ip = get_input("input/"+file_name+".in");
-    part1(ip.first, ip.second); // 
+    part1(ip.first, ip.second); // 1430536
     part2(ip.first, ip.second); // 
 }
