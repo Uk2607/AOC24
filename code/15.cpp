43d3ac8b9263d4015f0355b69997c59076352ee8
Utkarsh Kumawat <utkarsh2600@gmail.com>
2024-12-15 10:41:08 +0530

    day 15 input setup

--- code/15.cpp
+++ code/15.cpp
@@ -11,25 +11,32 @@
 #define ull unsigned long long int
 using namespace std;
 
-vector<string> get_input(string file_path) {
+pair<vector<string>, vector<string>> get_input(string file_path) {
     string line;
     vector<string>arr;
+    vector<string>instructions;
 
     ifstream file(file_path);
     if (!file.is_open()) {
         cerr << "Failed to open file: " << file_path << endl;
-        return arr;
+        return {arr, instructions};
     }
 
+    bool flag = false;
     while(getline(file, line)) {
-        arr.push_back(line);
+        if(line.length()==0) {
+            flag = true;
+            continue;
+        }
+        if(!flag) arr.push_back(line);
+        else instructions.push_back(line);
     }
     file.close();
 
-    return arr;
+    return {arr, instructions};
 }
 
-void print(vector<string>&arr) {
+void print(vector<string>&arr, vector<string>&ins) {
     cout<<"\n";
     for(string s: arr) {
         for(char c: s)
@@ -37,19 +44,26 @@ void print(vector<string>&arr) {
         cout<<"\n";
     }
     cout<<"\n";
+    for(string s: ins) {
+        for(char c: s)
+            cout<<c<<"";
+        cout<<"\n";
+    }
+    cout<<"\n";
 }
 
-void part1(vector<string>arr) {
+void part1(vector<string>arr, vector<string>ins) {
+    print(arr, ins);
     cout<<"PART1: "<<arr.size()<<"\n";
 }
 
-void part2(vector<string>arr) {
+void part2(vector<string>arr, vector<string>ins) {
     cout<<"PART2: "<<arr.size()<<"\n";
 }
 
 int main(int argc, char* argv[]) {
     string file_name = get_file_name(argc, argv, "__");
-    vector<string>arr  = get_input("input/"+file_name+".in");
-    part1(arr); // 
-    part2(arr); // 
+    pair<vector<string>, vector<string>>ip = get_input("input/"+file_name+".in");
+    part1(ip.first, ip.second); // 
+    part2(ip.first, ip.second); // 
 }
