cec6f93a42b986027fa122e2d87224614ca23a04
Utkarsh Kumawat <utkarsh2600@gmail.com>
2024-12-17 07:35:12 +0530

    d16 input setup

--- code/16.cpp
+++ code/16.cpp
@@ -11,6 +11,8 @@
 #define ull unsigned long long int
 using namespace std;
 
+vector<pair<int,int>>dir = {{0, 1}, {1, 0}, {0, -1}, {-1, 0}}; // right->down->left->up
+
 vector<string> get_input(string file_path) {
     string line;
     vector<string>arr;
@@ -39,7 +41,24 @@ void print(vector<string>&arr) {
     cout<<"\n";
 }
 
+bool inside(pair<int,int>p, int r, int c) {
+    return p.first>=0 && p.first<r && p.second>=0 && p.second<c;
+}
+
+int dfs(vector<string>&arr, vector<vector<bool>>&vis, pair<int,int>src, pair<int,int>dest, int r, int c, int &res) {
+    ;
+}
+
 void part1(vector<string>arr) {
+    int r = arr.size(), c = arr[0].length();
+    pair<int,int>src, dest;
+    for(int i=0; i<r; i++)
+        for(int j=-0; j<c; j++) {
+            if(arr[i][j] == 'S') src = {i, j};
+            if(arr[i][j] == 'E') dest = {i, j};
+        }
+    vector<vector<bool>>vis(r, vector<bool>(c, false));
+    int res = dfs(arr, vis, src, dest, r, c, 0);
     cout<<"PART1: "<<arr.size()<<"\n";
 }
 
