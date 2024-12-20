1c6677dd42ac46989f38fab98bf1bfae681add02
Utkarsh Kumawat <utkarsh2600@gmail.com>
2024-12-20 09:32:03 +0530

    day 18 19 input

---
+++ code/18.cpp
@@ -0,0 +1,55 @@
+#include "file_name.h"
+#include<iostream>
+#include<fstream>
+#include<sstream>
+#include<string>
+#include<vector>
+#include<map>
+#include<set>
+
+#define ll long long int
+#define ull unsigned long long int
+using namespace std;
+
+vector<string> get_input(string file_path) {
+    string line;
+    vector<string>arr;
+
+    ifstream file(file_path);
+    if (!file.is_open()) {
+        cerr << "Failed to open file: " << file_path << endl;
+        return arr;
+    }
+
+    while(getline(file, line)) {
+        arr.push_back(line);
+    }
+    file.close();
+
+    return arr;
+}
+
+void print(vector<string>&arr) {
+    cout<<"\n";
+    for(string s: arr) {
+        for(char c: s)
+            cout<<c<<"";
+        cout<<"\n";
+    }
+    cout<<"\n";
+}
+
+void part1(vector<string>arr) {
+    cout<<"PART1: "<<arr.size()<<"\n";
+}
+
+void part2(vector<string>arr) {
+    cout<<"PART2: "<<arr.size()<<"\n";
+}
+
+int main(int argc, char* argv[]) {
+    string file_name = get_file_name(argc, argv, "18");
+    vector<string>arr  = get_input("input/"+file_name+".in");
+    part1(arr); // 
+    part2(arr); // 
+}
