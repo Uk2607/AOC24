6bfbd28f14981c771f80be5e395e8f2f52596a24
Utkarsh Kumawat <utkarsh2600@gmail.com>
2024-12-22 20:20:15 +0530

    BFS

--- code/16.cpp
+++ code/16.cpp
@@ -45,8 +45,49 @@ bool inside(pair<int,int>p, int r, int c) {
     return p.first>=0 && p.first<r && p.second>=0 && p.second<c;
 }
 
-int dfs(vector<string>&arr, vector<vector<bool>>&vis, pair<int,int>src, pair<int,int>dest, int r, int c, int res) {
-    ;
+struct Coord {
+    int x, y, d;
+};
+
+
+int bfs(vector<string>&arr, pair<int,int>me, pair<int,int>dest, int r, int c) {
+    vector<vector<bool>>vis(r, vector<bool>(c, false));
+    queue<pair<Coord, int>>q;
+
+    int nx, ny, d;
+    Coord curr = {me.first, me.second, 0};
+    q.push({curr, 0});
+    vis[me.first][me.second] = true;
+
+    while(!q.empty()) {
+        curr = q.front().first;
+        int curr_dist = q.front().second;
+        if(curr.x == dest.first && curr.y == dest.second) return curr_dist;
+        q.pop();
+        // straight
+        d = curr.d;
+        nx = curr.x+dir[d].first, ny = curr.y+dir[d].second;
+        if(inside({nx, ny}, r, c) && arr[nx][ny]!='#' && !vis[nx][ny]) {
+            vis[nx][ny] = true;
+            q.push({{nx, ny, d}, curr_dist+1});
+        }
+        // left
+        d = (curr.d+3)%4;
+        nx = curr.x+dir[d].first, ny = curr.y+dir[d].second;
+        if(inside({nx, ny}, r, c) && arr[nx][ny]!='#' && !vis[nx][ny]) {
+            vis[nx][ny] = true;
+            q.push({{nx, ny, d}, curr_dist+1000});
+        }
+        // right
+        d = (curr.d+1)%4;
+        nx = curr.x+dir[d].first, ny = curr.y+dir[d].second;
+        if(inside({nx, ny}, r, c) && arr[nx][ny]!='#' && !vis[nx][ny]) {
+            vis[nx][ny] = true;
+            q.push({{nx, ny, d}, curr_dist+1000});
+        }
+        
+    }
+    return -1;
 }
 
 void part1(vector<string>arr) {
@@ -54,12 +95,11 @@ void part1(vector<string>arr) {
     pair<int,int>src, dest;
     for(int i=0; i<r; i++)
         for(int j=-0; j<c; j++) {
-            if(arr[i][j] == 'S') src = {i, j};
-            if(arr[i][j] == 'E') dest = {i, j};
+            if(arr[i][j] == 'S') { src = {i, j}; arr[i][j] = '.'; }
+            if(arr[i][j] == 'E') { dest = {i, j}; arr[i][j] = '.'; }
         }
-    vector<vector<bool>>vis(r, vector<bool>(c, false));
-    int res = dfs(arr, vis, src, dest, r, c, 0);
-    cout<<"PART1: "<<arr.size()<<"\n";
+    int res = bfs(arr, src, dest, r, c);
+    cout<<"PART1: "<<res<<"\n";
 }
 
 void part2(vector<string>arr) {
